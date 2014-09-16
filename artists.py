a059d3ec431e4950f6ddb1d2b90d2a08669d0339
dariengap <douglas.wilson@spsmail.cuny.edu>
2014-09-15 22:07:28 -0400

    New variables for length

--- artists.py
+++ artists.py
@@ -7,4 +7,8 @@ THE_GREAT_QUESTION = ('Michaelangelo. Leonardo. Rafael. Donatello. Turtles? '
                       'Creators of the great works? Both? You be the judge!')
 THE_GREAT_QUESTION.split('. ')
 STATEMENTS = THE_GREAT_QUESTION.split('. ')
-
+ARTISTS = STATEMENTS[0:4]
+len(ARTISTS)
+NUM_ARTISTS = len(ARTISTS)
+len(THE_GREAT_QUESTION)
+CHARACTERS = len(THE_GREAT_QUESTION)
